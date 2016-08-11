56951ee9b0366ef1507da93796e2f41999d28c35
魏华建 <weihuajian@weihuajiandeMacBook-Pro.local>
2016-08-11 10:29:23 +0800

    fix CornerTextView onDraw

--- app/src/main/java/example/com/universitytimetable/CornerTextView.java
+++ app/src/main/java/example/com/universitytimetable/CornerTextView.java
@@ -1,12 +1,9 @@
 package example.com.universitytimetable;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.graphics.drawable.BitmapDrawable;
-import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -25,20 +22,14 @@ public class CornerTextView extends TextView {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        setCornerBg(this.getMeasuredWidth(), this.getMeasuredHeight(), this);
-    }
 
-    public void setCornerBg(int w, int h, View v) {
-        Bitmap bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(bmp);
-
-        Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
+        Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(mBgColor);
         paint.setAlpha(180);
-        RectF rec = new RectF(0, 0, w, h);
-        c.drawRoundRect(rec, mCornerSize, mCornerSize, paint);
-        v.setBackground(new BitmapDrawable(getResources(), bmp));
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRoundRect(new RectF(0, 0, getMeasuredWidth(), getMeasuredHeight()), mCornerSize, mCornerSize, paint);
+
+        super.onDraw(canvas);
     }
 }
